4fc16c6dc016781772a65294e6b436b91a7f015f
Mohammad Taaha Ashraf <145143787+m-taaha@users.noreply.github.com>
2026-07-18 12:34:36 +0530

    (feat): add system links

--- app/components/sections/Systems/components/viewer/SystemActions.tsx
+++ app/components/sections/Systems/components/viewer/SystemActions.tsx
@@ -2,7 +2,7 @@ import { System } from "@/app/config/systems";
 
 import { FiArrowRight, FiGithub, FiExternalLink } from "react-icons/fi";
 import { SiGitlab } from "react-icons/si";
-import { SystemLink } from "../../cards/SystemLink";
+import { SystemLink } from "../../cards/SystemLink.tsx";
 
 
 
